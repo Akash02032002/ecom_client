97f18d82d14147050a04d10ab3fa6c54289be5be
Akash02032002 <aksahkumar02032002@gmail.com>
2025-05-26 08:21:00 +0530

    new client url update

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "https://ecom-server-0oqa.onrender.com/api" : "/api" , // replace with your API URL
+    // baseURL: import.meta.mode === "development" ? "https://ecom-server-0oqa.onrender.com/api" : "/api" ,
+    baseURL: import.meta.mode === "production" ? "https://ecom-server-0oqa.onrender.com/api" : "/api" , // replace with your API URL
     withCredentials: true, // send cookies with requests/server-side rendering
 });
 
