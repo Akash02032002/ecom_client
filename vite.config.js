7c9ac3159ecea0e892cecf4b5beea407208b953b
Akash02032002 <aksahkumar02032002@gmail.com>
2025-05-26 12:53:20 +0530

    server url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://ecom-server-0oqa.onrender.com",
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false, // 👈 This disables strict SSL validation for dev
       }
