6b7064752e68612051a6df9319ced76b0cac5836
Akash02032002 <aksahkumar02032002@gmail.com>
2025-05-29 21:55:13 +0530

    Hi Ana

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "https://ecom-server-0oqa.onrender.com/api" : "/api" , // replace with your API URL
-    withCredentials: true, // send cookies with requests/server-side rendering
+    baseURL: "https://ecom-server-0oqa.onrender.com/api", // Directly using your API URL
+    withCredentials: true, // send cookies with requests
 });
 
 export default axiosInstance;
